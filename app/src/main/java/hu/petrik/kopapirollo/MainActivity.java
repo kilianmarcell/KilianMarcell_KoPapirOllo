cb646ca2cfcb07bd3ae56a919e08231ee400bf29
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-10-20 17:47:04 +0200

    Inicializálás kész

--- app/src/main/java/hu/petrik/kopapirollo/MainActivity.java
+++ app/src/main/java/hu/petrik/kopapirollo/MainActivity.java
@@ -9,9 +9,29 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textViewTeValasztasod, textViewGepValasztasa, emberPontTextView, gepPontTextView;
+    private ImageView imageViewTeValasztasod, imageViewGepValasztasa;
+    private Button koGomb, papirGomb, olloGomb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    public void init() {
+        textViewTeValasztasod = findViewById(R.id.textViewTeValasztasod);
+        textViewGepValasztasa = findViewById(R.id.textViewGepValasztasa);
+        emberPontTextView = findViewById(R.id.emberPontTextView);
+        gepPontTextView = findViewById(R.id.gepPontTextView);
+
+        imageViewTeValasztasod = findViewById(R.id.imageViewTeValasztasod);
+        imageViewGepValasztasa = findViewById(R.id.imageViewGepValasztasa);
+
+        koGomb = findViewById(R.id.koGomb);
+        papirGomb = findViewById(R.id.papirGomb);
+        olloGomb = findViewById(R.id.olloGomb);
     }
 }
